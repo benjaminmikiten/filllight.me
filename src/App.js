641e76aec828454ef416a3b7c40221ece7355dc8
Benjamin Mikiten <benjaminmikiten@gmail.com>
2021-06-28 14:43:21 -0500

    make it work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
+import { setLightness } from 'polished';
 
-function App() {
+const StyledControl = styled(motion.div)`
+  /* position: absolute; */
+  /* top: 50%; */
+  /* left: 50%; */
+  /* transform: translate(-50%, -50%); */
+  height: 40px;
+  width: 40px;
+  border-radius: 100%;
+  background-color: black;
+  cursor: grab;
+  z-index: 999;
+`;
+
+const Panel = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  height: 100vh;
+  min-height: 100vh;
+  width: 100vw;
+  min-width: 100vw;
+  display: relative;
+`;
+
+const PanelBackground = styled(motion.div)`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right:  0;
+`;
+
+const App = () => {
+  const WARM = '#FF6C00';
+  const WHITE = '#FFFFFF';
+  const COOL = '#B5CDFF';
+
+  const panelRef = useRef();
+
+  const xMv = useMotionValue(0);
+  const yMv = useMotionValue(0);
+
+  const panelHorizHalf = panelRef.current?.offsetWidth * .5;
+  const panelVertHalf = panelRef.current?.offsetHeight * .5;
+
+  const opacity = useTransform(yMv, [panelVertHalf * -1, 0, panelVertHalf], [0, 0.5, 1]);
+  const warmth = useTransform(xMv, [panelHorizHalf * -1, 0, panelHorizHalf], [WARM, WHITE, COOL]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={panelRef}>
+      <Panel>
+        <PanelBackground style={{ background: warmth, opacity: opacity }} />
+        <StyledControl
+          style={{ x: xMv, y: yMv }}
+          dragMomentum={false}
+          drag
+          dragConstraints={panelRef}
+        />
+      </Panel>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from 'styled-normalize'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
